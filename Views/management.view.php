94e0255255b00b7487ccbe7d0ea1447022158d91
Dmgoj <domagoj.grgic@outlook.com>
2021-12-06 06:27:09 +0100

    Change remove button show logic

--- Views/management.view.php
+++ Views/management.view.php
@@ -38,7 +38,11 @@ $users_and_images= $images->showUsers();
     <table>
         <?php 
         foreach ($users_and_images as $img) {
-               echo "<tr><td> <img src='../" . $img['image'] . "' alt='PICTURE' width='100' height='100'><button>REMOVE</button><br>" . $img['username'] ."</td></tr>";
+              
+               echo "<tr><td> <img src='../" . $img['image'] . "' alt='PICTURE' width='100' height='100'><br>" . $img['username']; ?> 
+               <?php if ($_SESSION['user']==$img['username']){ echo"<form action='../management.php' method='post'>
+                <input type='hidden' name='image_to_delete' value='" . $img['image'] . "'><button name='remove'>REMOVE</button></form>";} ?> 
+               <?php "</td></tr>";
         }?>
     </table>
 </body>
